c1e0c01d375bb12e049819bfe368689612dfdadc
Kevin Guan <kzg5@cornell.edu>
2024-03-09 14:38:51 -0500

    50M

--- src/main/java/com/gkevin/Foo.java
+++ src/main/java/com/gkevin/Foo.java
@@ -11,7 +11,7 @@ public class Foo {
             list.add(i);
         }
 
-        for (int j = 0; j < 20000000; j++) {
+        for (int j = 0; j < 50000000; j++) {
             Iterator<Integer> iter = list.iterator();
             iter.next();
         }
